9747f8362e7466079a8a6ea485f74439447598e8
CereVenteo <36203330+CereVenteo@users.noreply.github.com>
2020-05-11 18:56:41 +0200

    gitignore correct

--- WAG_No_Sound/Assets/Scripts/UI/UIEventSounds.cs
+++ WAG_No_Sound/Assets/Scripts/UI/UIEventSounds.cs
@@ -9,28 +9,41 @@ using UnityEngine.EventSystems;
 
 public class UIEventSounds : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    public AK.Wwise.Event OnPointerDownSound;
-    public AK.Wwise.Event OnPointerUpSound;
-    public AK.Wwise.Event OnPointerEnterSound;
-    public AK.Wwise.Event OnPointerExitSound;
+    //public AK.Wwise.Event OnPointerDownSound;
+    //public AK.Wwise.Event OnPointerUpSound;
+    //public AK.Wwise.Event OnPointerEnterSound;
+    //public AK.Wwise.Event OnPointerExitSound;
+    [Header("Audio")]
+    public AudioClip ButtonEnterSound;
+    public AudioClip ButtonOverSound;
+    private AudioSource audio_source;
+
+    public void Start()
+    {
+        audio_source = GetComponent<AudioSource>();
+    }
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        OnPointerDownSound.Post(gameObject);
+        //OnPointerDownSound.Post(gameObject);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        OnPointerEnterSound.Post(gameObject);
+        audio_source.clip = ButtonEnterSound;
+        audio_source.Play();
+        //OnPointerEnterSound.Post(gameObject);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        OnPointerExitSound.Post(gameObject);
+        //audio_source.clip = MenuOpenSound;
+        //audio_source.Play();
+        //OnPointerExitSound.Post(gameObject);
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
-        OnPointerUpSound.Post(gameObject);
+        //OnPointerUpSound.Post(gameObject);
     }
 }
