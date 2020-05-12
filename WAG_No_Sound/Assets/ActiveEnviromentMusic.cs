7c2073f6c8b188a5abb2bc111736930d40f6fce6
Alejandro <paris.alejandro1@gmail.com>
2020-05-12 18:14:43 +0200

    day/night change music

--- WAG_No_Sound/Assets/ActiveEnviromentMusic.cs
+++ WAG_No_Sound/Assets/ActiveEnviromentMusic.cs
@@ -7,6 +7,7 @@ public class ActiveEnviromentMusic : MonoBehaviour
     public AudioSource father;
     public AudioClip music;
     public AudioClip nightmusic;
+    public AudioClip song;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,14 +17,23 @@ public class ActiveEnviromentMusic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (GameManager.TimeOfDay > 20 || GameManager.TimeOfDay < 6)
+        {
+            song = nightmusic;     
+        }
+        else
+        {
+            song = music;
+        }
         
+
     }
-    private void OnTriggerEnter(Collider other)
+    private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
         {
             father.loop = true;
-            father.clip = music;
+            father.clip = song;
             father.Play();
         }
     }
